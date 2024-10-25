86e37f26170c208aceb65df74ca5266a59d628f8
shahzad9294 <shahzadmunna746@gmail.com>
2024-10-26 01:48:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['+923442392492', '+923442392492']
+global.prems = ['+923442392492', '+923442392492', '+923442392492']
+global.allowed = ['+923442392492', '+923442392492', '+923442392492']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
